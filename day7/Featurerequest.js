321a69f83b5c2b2117a5af2275e2d94443b182f2
swapnilAngarkhe <swapnilang21@gmail.com>
2024-07-19 19:31:51 +0530

    featuredRequest

--- day7/Featurerequest.js
+++ day7/Featurerequest.js
@@ -1,13 +1,14 @@
 let book1 = {
     author: " Maggie O'Farrell",
     name: "Hamnet",
-    year: 2020
+    year: 2020,
+    updatedYear: function (year) {
+        this.year = year;
+      }
 };
 console.log(book1);
-book1.getDetails = function() {
-    return `${this.name} by ${this.author}`;
-}
-console.log(book1.getDetails());
+book1.updatedYear(2030);
+
 
 
 let Library = {
@@ -31,11 +32,19 @@ let Library = {
     ]
 };
 
-for (const property in book1) {
-    console.log(`${property}: ${book1[property]}`);
-}
-console.log(Object.keys(book1));
-console.log(Object.values(book1));
 
+const books=
+            {
+                title: "After you'd go",
+                author: "Maggie O'Farrell",
+                year: 2000
+            }
+
+for (const property in books) {
+    console.log(`${property}: ${books[property]}`);
+}
+console.log(Object.keys(books));
+console.log(Object.values(books));
+       
 
 
